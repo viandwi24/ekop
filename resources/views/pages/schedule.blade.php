cbc1092956c4b940878d07c5da475b964810c84b
viandwi24 <fiandwi0424@gmail.com>
2021-10-22 23:27:59 +0700

    fix: schedule

--- resources/views/pages/schedule.blade.php
+++ resources/views/pages/schedule.blade.php
@@ -69,7 +69,7 @@
                                             <td>
                                                 @if ($item->client_name)
                                                     {{ $item->client_name }}
-                                                @elseif ($item->personal_data->cooperative->name)
+                                                @elseif ($item->personal_data->cooperative)
                                                     {{ $item->personal_data->cooperative->name }}
                                                 @else
                                                     {{ $item->personal_data->name }}
